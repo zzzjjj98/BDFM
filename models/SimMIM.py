7290581af35ba594bc404e011ba698b3c0a718bf
zzzjjj98 <151014785+zzzjjj98@users.noreply.github.com>
2025-05-26 17:00:50 +0800

    Update SimMIM.py

--- models/SimMIM.py
+++ models/SimMIM.py
@@ -1,10 +1,12 @@
-"""Jiatian Zhang
-zjt20174025@mail.ustc.edu.cn
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Licensed under the CC BY-NC 4.0 License (https://creativecommons.org/licenses/by-nc/4.0/)
+# Modified by Jiatian Zhang, 2025.
+# --------------------------------------------------------
 Reference:
 https://github.com/microsoft/SimMIM/blob/main/models/simmim.py
 https://github.com/facebookresearch/mae/blob/main/models_mae.py
 https://github.com/Project-MONAI/MONAI/blob/b61db797e2f3bceca5abbaed7b39bb505989104d/monai/networks/nets/vit.py
-"""
+# --------------------------------------------------------
 
 import logging
 from typing import Sequence, Union
